f129c868fd92da0d3b1bc7dd0ae99629234bdcf6
aseyedia <arta.seyedian@gmail.com>
2024-08-14 15:12:33 -0400

    Fixing the Browse menu bar here https://github.com/PEDSnet/PEDSpace/issues/13

--- src/themes/PEDSpace/app/shared/comcol-page-browse-by/comcol-page-browse-by.component.ts
+++ src/themes/PEDSpace/app/shared/comcol-page-browse-by/comcol-page-browse-by.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { ComcolPageBrowseByComponent as BaseComponent} from '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component';
+/* eslint-disable max-classes-per-file */
+import {
+  AsyncPipe,
+  NgForOf,
+  NgIf,
+} from '@angular/common';
+import {
+  Component,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
-/**
- * A component to display the "Browse By" section of a Community or Collection page
- * It expects the ID of the Community or Collection as input to be passed on as a scope
- */
-@Component({
-  selector: 'ds-comcol-page-browse-by',
-  // styleUrls: ['./comcol-page-browse-by.component.scss'],
-  styleUrls: ['../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
-  // templateUrl: './comcol-page-browse-by.component.html'
-  templateUrl: '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.html'
+import { ComcolPageBrowseByComponent as BaseComponent } from '../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component';
+
+@Pipe({
+  name: 'filterType',
+  standalone: true,
 })
-export class ComcolPageBrowseByComponent extends BaseComponent {}
+export class FilterTypePipe implements PipeTransform {
+  transform(options: any[]): any[] {
+    // this is where we crudely filter out unwanted metadata fields by dc schema name ie. 'dc.type' or 'browse.comcol.by.srsc'
+    return options.filter(option => !option.id.includes('type') && !option.id.includes('srsc'));
+  }
+}
+
+@Component({
+  selector: 'ds-themed-comcol-page-browse-by',
+  styleUrls: ['../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
+  templateUrl: './comcol-page-browse-by.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgForOf,
+    RouterLink,
+    RouterLinkActive,
+    TranslateModule,
+    AsyncPipe,
+    NgIf,
+    FilterTypePipe,
+  ],
+})
+export class ComcolPageBrowseByComponent extends BaseComponent {
+}
