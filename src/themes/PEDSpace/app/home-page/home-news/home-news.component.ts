d8249da65ddeac46777b84ebc3fb231fec9c03e4
aseyedia <arta.seyedian@gmail.com>
2024-10-01 16:53:29 -0400

    added dev notice warning

--- src/themes/PEDSpace/app/home-page/home-news/home-news.component.ts
+++ src/themes/PEDSpace/app/home-page/home-news/home-news.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CommonModule } from '@angular/common';
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
 
 @Component({
   selector: 'ds-themed-home-news',
   styleUrls: ['./home-news.component.scss'],
-  // styleUrls: ['../../../../../app/home-page/home-news/home-news.component.scss'],
   templateUrl: './home-news.component.html',
-  // templateUrl: '../../../../../app/home-page/home-news/home-news.component.html',
   standalone: true,
+  imports: [BaseComponent, CommonModule],
 })
+export class HomeNewsComponent extends BaseComponent implements OnInit {
+  isProduction: boolean = environment.production; 
+  ipAddressMatch: boolean = false; // For storing if IP matches
+  showDevOnProdMessage: boolean = false;
 
-/**
- * Component to render the news section on the home page
- */
-export class HomeNewsComponent extends BaseComponent {}
+  private targetIpAddress = 'pedsdspaceprod.research.chop.edu'; // Production IP
 
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+    // Check if the environment's host matches the target IP
+    this.ipAddressMatch = (environment.rest.host === this.targetIpAddress);
+  }
+}
