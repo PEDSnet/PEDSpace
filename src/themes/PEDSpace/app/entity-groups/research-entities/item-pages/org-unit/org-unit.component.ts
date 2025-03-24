aafb75df1444f0ef4bb3f8c286ee7eb2ca00ff76
aseyedia <arta.seyedian@gmail.com>
2025-03-24 10:14:24 -0400

    fixing themed OrgUnit installation

--- src/themes/PEDSpace/app/entity-groups/research-entities/item-pages/org-unit/org-unit.component.ts
+++ src/themes/PEDSpace/app/entity-groups/research-entities/item-pages/org-unit/org-unit.component.ts
@@ -10,7 +10,7 @@ import { ViewMode } from 'src/app/core/shared/view-mode.model';
 import { GenericItemPageFieldComponent } from 'src/app/item-page/simple/field-components/specific-field/generic/generic-item-page-field.component';
 import { ItemPageImgFieldComponent } from 'src/app/item-page/simple/field-components/specific-field/img/item-page-img-field.component';
 import { ThemedItemPageTitleFieldComponent } from 'src/app/item-page/simple/field-components/specific-field/title/themed-item-page-field.component';
-import { ItemComponent } from 'src/app/item-page/simple/item-types/shared/item.component';
+// import { ItemComponent } from 'src/app/item-page/simple/item-types/shared/item.component';
 import { TabbedRelatedEntitiesSearchComponent } from 'src/app/item-page/simple/related-entities/tabbed-related-entities-search/tabbed-related-entities-search.component';
 import { RelatedItemsComponent } from 'src/app/item-page/simple/related-items/related-items-component';
 import { DsoEditMenuComponent } from 'src/app/shared/dso-page/dso-edit-menu/dso-edit-menu.component';
@@ -18,17 +18,18 @@ import { MetadataFieldWrapperComponent } from 'src/app/shared/metadata-field-wra
 import { listableObjectComponent } from 'src/app/shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ThemedResultsBackButtonComponent } from 'src/app/shared/results-back-button/themed-results-back-button.component';
 import { ThemedThumbnailComponent } from 'src/app/thumbnail/themed-thumbnail.component';
+import { Context } from '../../../../../../../app/core/shared/context.model';
+import { OrgUnitComponent as BaseComponent } from 'src/app/entity-groups/research-entities/item-pages/org-unit/org-unit.component';
 
-@listableObjectComponent('OrgUnit', ViewMode.StandalonePage)
+@listableObjectComponent('OrgUnit', ViewMode.StandalonePage, Context.Any, 'PEDSpace')
 @Component({
   selector: 'ds-org-unit',
   styleUrls: ['./org-unit.component.scss'],
+  // styleUrls: ['../../../../../../../app/entity-groups/research-entities/item-pages/person/person.component.scss'],
   templateUrl: './org-unit.component.html',
+  // templateUrl: '../../../../../../../app/entity-groups/research-entities/item-pages/person/person.component.html',
   standalone: true,
   imports: [NgIf, ThemedResultsBackButtonComponent, ThemedItemPageTitleFieldComponent, DsoEditMenuComponent, MetadataFieldWrapperComponent, ThemedThumbnailComponent, GenericItemPageFieldComponent, RelatedItemsComponent, RouterLink, TabbedRelatedEntitiesSearchComponent, AsyncPipe, TranslateModule, ItemPageImgFieldComponent],
 })
-/**
- * The component for displaying metadata and relations of an item of the type Organisation Unit
- */
-export class OrgUnitComponent extends ItemComponent {
+export class OrgUnitComponent extends BaseComponent {
 }
