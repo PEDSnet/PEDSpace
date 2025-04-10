a25b955cb5aaa9d223d7cedae0a42b0c6a5b935f
aseyedia <arta.seyedian@gmail.com>
2025-04-10 10:08:31 -0400

    Finished up loose ends on Documentation SIV #91

--- src/app/item-page/simple/field-components/specific-field/generic/generic-item-page-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/generic/generic-item-page-field.component.ts
@@ -1,7 +1,7 @@
 import { AsyncPipe } from '@angular/common';
 import {
   Component,
-  Input,
+  Input
 } from '@angular/core';
 
 import { Item } from '../../../../../core/shared/item.model';
@@ -10,7 +10,7 @@ import { ItemPageFieldComponent } from '../item-page-field.component';
 
 @Component({
   selector: 'ds-generic-item-page-field',
-  templateUrl: '../item-page-field.component.html',
+  templateUrl: './generic-item-page-field.component.html',
   standalone: true,
   imports: [MetadataValuesComponent, AsyncPipe],
 })
@@ -73,5 +73,50 @@ export class GenericItemPageFieldComponent extends ItemPageFieldComponent {
    * Flag to indicate if the DQ check requirement is enabled
    */
   @Input() isDQCheckRequirement = false;
+
+  @Input() isDateRange = false;
+
+
+  // In item-page-field.component.ts, add this method
+  getCombinedMetadataValues() {
+    if (!this.item || !this.fields || this.fields.length === 0) {
+      return [];
+    }
+
+    if (this.fields.length === 1) {
+      return this.item.allMetadata(this.fields);
+    }
+
+    const combinedValues = [];
+    const valuesMap = {};
+
+    this.fields.forEach(field => {
+      const values = this.item.allMetadata([field]);
+      if (values && values.length > 0) {
+        valuesMap[field] = values;
+      }
+    });
+
+    if (this.fields.length === 2) {
+      const field1 = this.fields[0];
+      const field2 = this.fields[1];
+
+      if (valuesMap[field1] && valuesMap[field1].length > 0 &&
+        valuesMap[field2] && valuesMap[field2].length > 0) {
+        combinedValues.push({
+          value: valuesMap[field1][0].value + this.separator + valuesMap[field2][0].value,
+          language: valuesMap[field1][0].language,
+          authority: null,
+          place: -1
+        });
+      } else if (valuesMap[field1] && valuesMap[field1].length > 0) {
+        combinedValues.push(valuesMap[field1][0]);
+      } else if (valuesMap[field2] && valuesMap[field2].length > 0) {
+        combinedValues.push(valuesMap[field2][0]);
+      }
+    }
+
+    return combinedValues;
+  }
 
 }
