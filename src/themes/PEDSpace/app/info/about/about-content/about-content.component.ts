eb145495baa769412da6996348e6a93f4016ba35
aseyedia <arta.seyedian@gmail.com>
2025-10-09 16:26:24 -0400

    Add extensive About page content #7

--- src/themes/PEDSpace/app/info/about/about-content/about-content.component.ts
+++ src/themes/PEDSpace/app/info/about/about-content/about-content.component.ts
@@ -1,17 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ds-about-content',
   templateUrl: './about-content.component.html',
   styleUrls: ['./about-content.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslateModule, NgbAccordionModule],
+  imports: [CommonModule, RouterModule],
 })
 /**
- * Component displaying the content of the About page
+ * Component displaying the content of the PEDSpace Knowledge Bank About page
  */
 export class AboutContentComponent {
+  // Component logic can be added here if needed in the future
 }
