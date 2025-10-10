699ca8198aa39b72cc7a1527bb0df009a3761968
aseyedia <arta.seyedian@gmail.com>
2025-10-10 10:36:49 -0400

    Animate hero section background in About page #7

--- src/themes/PEDSpace/app/info/about/about-content/about-content.component.ts
+++ src/themes/PEDSpace/app/info/about/about-content/about-content.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -12,6 +16,16 @@ import { RouterModule } from '@angular/router';
 /**
  * Component displaying the content of the PEDSpace Knowledge Bank About page
  */
-export class AboutContentComponent {
-  // Component logic can be added here if needed in the future
+export class AboutContentComponent implements OnInit {
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    // Set a random starting position for the hero gradient animation (0-20 seconds)
+    const randomStart = Math.random() * 20;
+    const heroSection = this.elementRef.nativeElement.querySelector('.hero-section');
+    if (heroSection) {
+      heroSection.style.setProperty('--random-start', randomStart.toString());
+    }
+  }
 }
