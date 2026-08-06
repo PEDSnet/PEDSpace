ac164578c92695636ed96268230bdcb3fa517285
Annabel <pinkneya1@chop.edu>
2026-08-06 09:44:09 -0400

    truncate long breadcrumbs

--- src/themes/PEDSpace/app/breadcrumbs/breadcrumbs.component.ts
+++ src/themes/PEDSpace/app/breadcrumbs/breadcrumbs.component.ts
@@ -2,12 +2,14 @@ import {
   AsyncPipe,
   NgFor,
   NgIf,
+  SlicePipe,
   NgTemplateOutlet,
 } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
+
 
 import { BreadcrumbsComponent as BaseComponent } from '../../../../app/breadcrumbs/breadcrumbs.component';
 import { VarDirective } from '../../../../app/shared/utils/var.directive';
@@ -21,7 +23,7 @@ import { VarDirective } from '../../../../app/shared/utils/var.directive';
   styleUrls: ['./breadcrumbs.component.scss'],
   // styleUrls: ['../../../../app/breadcrumbs/breadcrumbs.component.scss'],
   standalone: true,
-  imports: [VarDirective, NgIf, NgTemplateOutlet, NgFor, RouterLink, NgbTooltipModule, AsyncPipe, TranslateModule],
+  imports: [VarDirective, NgIf, NgTemplateOutlet, NgFor, RouterLink, NgbTooltipModule, AsyncPipe, TranslateModule, SlicePipe],
 })
 export class BreadcrumbsComponent extends BaseComponent {
 }
